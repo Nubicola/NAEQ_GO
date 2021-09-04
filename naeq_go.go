399a24ad6a63a978946036ed725a27eb6a7cabeb
Nubicola Linux Laptop <git@maitri27.33mail.com>
2021-09-04 13:13:04 +0200

    add the corpus for testing and benchmarking routines. Add benchmarking routines itself. add modulus-based eq.

--- naeq_go.go
+++ naeq_go.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
+
+// adding a different way to calculate (testing it later)
+// let's test if the map-based method is significantly slower than the math-based method
 
 // this is the main map that translates characters to values
 
 var eqMap map[rune]int
 
-func main() {
+// initializes the map
+func InitMyMap() {
 	//this could be done in a much nicer way, but for now, this does the trick I guess
 	vals := [26]int{1, 20, 13, 6, 25, 18, 11, 4, 23, 16, 9, 2, 21, 14, 7, 26, 19, 12, 5, 24, 17, 10, 3, 22, 15, 8}
 	chs := [26]rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
@@ -17,19 +22,37 @@ func main() {
 	for i := 0; i < len(vals); i++ {
 		eqMap[chs[i]] = vals[i]
 	}
-	fmt.Println(eqMap)
-	fmt.Println("Sum of 'hello', ", eqalculate("hello"))
 }
 
-// calculates EQ Value of (exactly) 1 word
-func eqalculate(word string) int {
+func main() {
+	InitMyMap()
+	//fmt.Println(eqMap)
+	fmt.Println("Sum of 'hello', ", EQalculate("hello"))
+	fmt.Println("Sum of hello-mod", EQalculateMod("hello"))
+}
+
+// calculate EQ of word using % operator
+func EQalculateMod(word string) int {
+	value := 0
+	for _, c := range word {
+		if unicode.IsLower(c) {
+			value += int(c-'a')*19%26 + 1
+		}
+	}
+	return value
+}
+
+// calculates EQ Value of (exactly) 1 word using a map
+func EQalculate(word string) int {
 	splitStrings := strings.Split(word, " ")
 	if len(splitStrings) > 1 {
 		panic("this function calculates 1 word only!")
 	}
 	oneWord := strings.ToLower(splitStrings[0])
+	//fmt.Printf("receive %s lowered and split to %s\n", word, oneWord)
 	eq := 0
 	for r := range oneWord {
+		//fmt.Printf("rune %v value %v\n", oneWord[r], eqMap[rune(oneWord[r])])
 		eq += eqMap[rune(oneWord[r])]
 	}
 	return eq
---
+++ naeq_go_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func BenchmarkEQalculate(b *testing.B) {
+	data := getCorpusStrings(b)
+	// this is needed so the EQ Value map is filled out
+	InitMyMap()
+	words := regexp.MustCompile(`[\w'\-]+`).FindAll(data, -1)
+
+	for _, wordSlice := range words {
+		word := strings.ToLower(string(wordSlice))
+		//fmt.Println(word)
+		_ = EQalculate(word)
+		//fmt.Println(v)
+	}
+}
+
+func BenchmarkEQalculateMod(b *testing.B) {
+	data := getCorpusStrings(b)
+	words := regexp.MustCompile(`[\w'\-]+`).FindAll(data, -1)
+	for _, wordSlice := range words {
+		word := strings.ToLower(string(wordSlice))
+		_ = EQalculateMod(word)
+	}
+
+}
+
+func getCorpusStrings(b *testing.B) []byte {
+	data, err := ioutil.ReadFile("corpus/lib10.txt")
+	if err != nil {
+		b.Error("Unable to open file")
+	}
+	data2, err := ioutil.ReadFile("corpus/lib65.txt")
+	if err != nil {
+		b.Error("unable to open next file")
+	}
+	data3, err := ioutil.ReadFile("corpus/lib7a.txt")
+	if err != nil {
+		b.Error("third file not available")
+	}
+	data = append(data, data2...)
+	data = append(data, data3...)
+	return (data)
+	//return (append(data, data2...))
+
+}
